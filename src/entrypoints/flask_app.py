c28d6332b435aa3feb28163db6a94341c870d2e4
DonHesus <grzegorzchlechowicz@gmail.com>
2022-11-07 18:33:11 +0100

    Add ORM connection

--- src/entrypoints/flask_app.py
+++ src/entrypoints/flask_app.py
@@ -1,7 +1,23 @@
-from flask import Flask
 import logging
+from flask import Flask
 
-app = Flask(__name__)
+import settings
+from src.adapters.data.db_sqlalchemy import db
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = settings.Settings.database_url
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    create_app()

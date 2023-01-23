d6ac9a6ec089cca9c50f5cec5cd4a156b62dea80
DonHesus <grzegorzchlechowicz@gmail.com>
2023-01-23 09:20:29 +0100

    Health monitor updated

--- src/entrypoints/health_monitor.py
+++ src/entrypoints/health_monitor.py
@@ -1,4 +1,5 @@
 import ipaddress
+import json
 import os
 import sys
 import time
@@ -22,9 +23,9 @@ class HealthStatus:
 if __name__ == '__main__':
     while True:
         try:
-            proc = subprocess.Popen("ip -4 addr show wlo1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'",
-                                    stdout=subprocess.PIPE, shell=True)
-            mac_address = proc.communicate()[0].decode('utf-8').strip()
+            proc = subprocess.Popen("ip -j addr show wlo1", stdout=subprocess.PIPE, shell=True)
+            output_dict = json.loads(proc.communicate()[0].decode())
+            mac_address = output_dict["address"]
             post(f"{os.environ['SEVER_ADDRESS']}/health/{mac_address}")
             time.sleep(360)
         except KeyboardInterrupt:
---
+++ src/services/image_manipulation.py
@@ -0,0 +1,2 @@
+def create_new_image(golden_image, new_image_id):
+    pass

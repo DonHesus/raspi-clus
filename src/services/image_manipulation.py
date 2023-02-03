d6e24cc2fd27b1a7938f4ab14ca34635e813929f
DonHesus <grzegorzchlechowicz@gmail.com>
2023-02-03 15:51:09 +0100

    end_of_the_week_push

--- src/services/image_manipulation.py
+++ src/services/image_manipulation.py
@@ -8,6 +8,8 @@ def create_new_distributed_image(golden_image_path, new_image_id):
     cp_proc = subprocess.Popen(f"cp -ax {golden_image_path} {Settings.image_store}/{new_image_id}",
                                stdout=subprocess.PIPE, shell=True)
     cp_proc.communicate()
+    
+    _edit_cmdline()
     return f"{Settings.image_store}/{new_image_id}"
 
 
@@ -15,3 +17,7 @@ def add_new_golden_image(system_files_path: Path, boot_dir_path: Path):
     cp_proc = subprocess.Popen(f"cp -ax {system_files_path} {boot_dir_path}", stdout=subprocess.PIPE,
                                shell=True)
     cp_proc.communicate()
+
+
+def _edit_cmdline():
+    pass

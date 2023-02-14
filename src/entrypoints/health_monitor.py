e6a2cd0427c29f2b26972c5ebab079e996511c57
DonHesus <grzegorzchlechowicz@gmail.com>
2023-02-14 13:49:06 +0100

    Health monitor refactor

--- src/entrypoints/health_monitor.py
+++ src/entrypoints/health_monitor.py
@@ -1,33 +1,78 @@
-import ipaddress
 import json
-import os
+import logging
 import sys
 import time
-import uuid
-from dataclasses import dataclass
-from dataclasses import asdict
 import subprocess
+from dataclasses import dataclass
 from requests import post
+
+logger = logging.getLogger("health_monitor")
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                              datefmt='%Y-%m-%d %H:%M:%S')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler("/var/log/health_monitor.log")
+fh.setFormatter(formatter)
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+CONFIG_FILE = "/bin/health_monitor_config.json"
+
+
+def set_config_varialbes():
+    with open(CONFIG_FILE, "r") as config:
+        config_dict = json.loads(config.read())
+        setup = Settings(FIRST_BOOT=config_dict["first_boot"], SERVER_ADDRESS=config_dict["server_address"])
+
+    return setup
+
+
+def update_config_file(current_conf):
+    current_conf.FIRST_BOOT = 0
+    with open(CONFIG_FILE, "w") as config_file:
+        config_file.write(json.dumps(current_conf.as_dict()))
 
 
 @dataclass
-class HealthStatus:
-    id: uuid.UUID
-    network: ipaddress.ip_address
-    alive: bool
+class Settings:
+    FIRST_BOOT: int
+    SERVER_ADDRESS: str
 
     def as_dict(self):
-        return asdict(self)
+        return {"first_boot": self.FIRST_BOOT,
+                "server_address": self.SERVER_ADDRESS}
 
 
 if __name__ == '__main__':
+    try:
+        config = set_config_varialbes()
+
+        if int(config.FIRST_BOOT):
+            proc = subprocess.Popen("sudo /bin/rm -v /etc/ssh/ssh_host_*;"
+                                    " sudo dpkg-reconfigure openssh-server;"
+                                    " sudo systemctl restart ssh;"
+                                    " /etc/init.d/ssh restart",
+                                    stdout=subprocess.PIPE, shell=True)
+            proc.communicate()
+
+            update_config_file(config)
+    except Exception as msg:
+        logger.info(f"Error {msg}")
+        sys.exit(1)
     while True:
         try:
-            proc = subprocess.Popen("ip -j addr show wlo1", stdout=subprocess.PIPE, shell=True)
+            proc = subprocess.Popen("ip -j addr show eth0", stdout=subprocess.PIPE, shell=True)
             output_dict = json.loads(proc.communicate()[0].decode())
-            mac_address = output_dict["address"]
-            post(f"{os.environ['SEVER_ADDRESS']}/health/{mac_address}")
+            mac_address = output_dict[0]["address"]
+            logger.info(f"Sending status to the server, address: {config.SERVER_ADDRESS}/health/{mac_address}")
+            try:
+                post(f"{config.SERVER_ADDRESS}/health/{mac_address}")
+            except Exception as error:
+                logger.info(f"Error occured error msg: {error.args}")
+                sys.exit(0)
             time.sleep(360)
         except KeyboardInterrupt:
-            print("Exiting health_monitor")
+            logger.info("Exiting health_monitor")
             sys.exit(0)
+
+
+
+
